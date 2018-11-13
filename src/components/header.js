c8261e309a1ba401ad3c3697a8b2cce29ff9e5bb
Kim Thuy Tu <KimThuyTu@Kims-MacBook.local>
2018-11-13 15:32:14 +0700

    fixed the images and moved to static folder

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
       <Link to="/about">About</Link>
-      <Link to="/saigon">Region</Link>
+      <Link to="/journal">Journal</Link>
       <Link to="/instagram">Instagram</Link>
       <Link to="/contact"><button>Contact</button></Link>
   </div>
